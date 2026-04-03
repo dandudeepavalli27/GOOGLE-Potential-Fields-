dfd3ffc4a9aea059f8cecd86166c454a8539f6ae
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 11:58:58 +0530

    Create Potential Fields .py

---
+++ Potential Fields .py
@@ -0,0 +1,13 @@
+# SESSION 25 – Potential Fields (Net Force Calculation)
+
+# Step 1: Input values
+attractive = 12
+repulsive = 5
+
+# Step 2: Compute net force
+force = attractive - repulsive
+
+# Step 3: Display result
+print("Net Force =", force)
+
+print("\nProgram Executed Successfully")
